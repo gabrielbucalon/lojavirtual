f4c4c0b0419aaac665d59d1981880e6005c40c83
gabriel.bucalon <bucalongabriell@gmail.com>
2020-06-16 10:15:44 -0300

    <feature> - tabs category lits and grid

--- lib/src/screens/category_screen.dart
+++ lib/src/screens/category_screen.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/src/data/product_data.dart';
+import 'package:lojavirtual/src/tiles/product_tiles.dart';
 
 class CategoryScreen extends StatelessWidget {
   final DocumentSnapshot snapshot;
@@ -11,18 +13,52 @@ class CategoryScreen extends StatelessWidget {
     return DefaultTabController(
         length: 2,
         child: Scaffold(
-          appBar: AppBar(
-            title: Text(snapshot.data["title"]),
-            centerTitle: true,
-            bottom: TabBar(indicatorColor: Colors.white, tabs: <Widget>[
-              Tab(icon: Icon(Icons.grid_on)),
-              Tab(icon: Icon(Icons.list))
-            ]),
-          ),
-          body: TabBarView(physics: NeverScrollableScrollPhysics(), children: [
-            Container(color: Colors.red),
-            Container(color: Colors.green),
-          ]),
-        ));
+            appBar: AppBar(
+              title: Text(snapshot.data["title"]),
+              centerTitle: true,
+              bottom: TabBar(indicatorColor: Colors.white, tabs: <Widget>[
+                Tab(icon: Icon(Icons.grid_on)),
+                Tab(icon: Icon(Icons.list))
+              ]),
+            ),
+            body: FutureBuilder<QuerySnapshot>(
+                future: Firestore.instance
+                    .collection("products")
+                    .document(snapshot.documentID)
+                    .collection("items")
+                    .getDocuments(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(child: CircularProgressIndicator());
+                  } else {
+                    return TabBarView(
+                        physics: NeverScrollableScrollPhysics(),
+                        children: [
+                          GridView.builder(
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                      crossAxisCount: 2,
+                                      mainAxisSpacing: 4,
+                                      crossAxisSpacing: 4,
+                                      childAspectRatio: 0.65),
+                              itemCount: snapshot.data.documents.length,
+                              itemBuilder: (context, index) {
+                                return ProductTiles(
+                                    type: "grid",
+                                    data: ProductData.fromDocument(
+                                        snapshot.data.documents[index]));
+                              }),
+                          ListView.builder(
+                              padding: EdgeInsets.all(4),
+                              itemCount: snapshot.data.documents.length,
+                              itemBuilder: (context, index) {
+                                return ProductTiles(
+                                    type: "list",
+                                    data: ProductData.fromDocument(
+                                        snapshot.data.documents[index]));
+                              })
+                        ]);
+                  }
+                })));
   }
 }
---
+++ lib/src/tiles/product_tiles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/src/data/product_data.dart';
+
+class ProductTiles extends StatelessWidget {
+  final String type;
+  final ProductData data;
+
+  const ProductTiles({Key key, this.type, this.data}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
