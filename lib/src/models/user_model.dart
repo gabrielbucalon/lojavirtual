02688d3853814e1c1e467990dc2c086b33ecae88
gabriel.bucalon <bucalongabriell@gmail.com>
2020-06-25 15:22:44 -0300

    <feature> - rules sign Up and save user Firebase

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -1,16 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class UserModel extends Model{
+class UserModel extends Model {
+  FirebaseAuth _auth = FirebaseAuth.instance;
 
+  FirebaseUser _firebaseUser;
+  Map<String, dynamic> userData = Map();
   // Usuario atual
 
   bool isLoading = false;
 
-  void signUp(){
+  void signUp(
+      {@required Map<String, dynamic> userData,
+      @required String pass,
+      @required VoidCallback onSucess,
+      @required VoidCallback onFail}) {
+    isLoading = true;
+    notifyListeners();
 
+    _auth
+        .createUserWithEmailAndPassword(
+            email: userData["email"], password: pass)
+        .then((user) async {
+      _firebaseUser = user;
+      await _saveUserData(userData);
+      onSucess();
+      isLoading = false;
+      notifyListeners();
+    }).catchError((e) {
+      onFail();
+      isLoading = false;
+      notifyListeners();
+    });
   }
 
-  void signIn() async{
+  void signIn() async {
     isLoading = true;
     notifyListeners();
 
@@ -20,11 +47,15 @@ class UserModel extends Model{
     notifyListeners();
   }
 
-  void recoverPassword(){
-    
-  }
+  void recoverPassword() {}
 
-  void isLoggedIn(){
-    
+  void isLoggedIn() {}
+
+  Future<Null> _saveUserData(Map<String, dynamic> userData) {
+    this.userData = userData;
+    Firestore.instance
+        .collection("users")
+        .document(_firebaseUser.uid)
+        .setData(userData);
   }
 }
